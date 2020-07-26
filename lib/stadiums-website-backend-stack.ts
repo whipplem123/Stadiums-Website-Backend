a77fe817f5e8c0e44f16e4307e9998c1bfe1dd4c
Matt Whipple <whipplem123@gmail.com>
2020-07-26 01:18:11 -0400

    Created DDB StadiumsMapTable in CDK

--- lib/stadiums-website-backend-stack.ts
+++ lib/stadiums-website-backend-stack.ts
@@ -1,9 +1,16 @@
 import * as cdk from '@aws-cdk/core';
+import { Table, AttributeType } from '@aws-cdk/aws-dynamodb';
 
 export class StadiumsWebsiteBackendStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    this.createResources();
+  }
 
-    // The code that defines your stack goes here
+  private createResources() {
+    new Table(this, 'StadiumsMapTable', {
+      tableName: 'StadiumsMapTable',
+      partitionKey: { name: 'teamId', type: AttributeType.STRING }
+    });
   }
 }
--- test/stadiums-website-backend.test.ts
+++ test/stadiums-website-backend.test.ts
@@ -1,13 +1,17 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import { expect, haveResource } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as StadiumsWebsiteBackend from '../lib/stadiums-website-backend-stack';
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new StadiumsWebsiteBackend.StadiumsWebsiteBackendStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+describe('CDK tests', () => {
+  let app: cdk.App;
+  let stack: StadiumsWebsiteBackend.StadiumsWebsiteBackendStack;
+
+  beforeAll(() => {
+    app = new cdk.App();
+    stack = new StadiumsWebsiteBackend.StadiumsWebsiteBackendStack(app, 'TestStack');
+  });
+
+  test('should create DDB table', () => {
+    expect(stack).to(haveResource('AWS::DynamoDB::Table'));
+  });
 });
